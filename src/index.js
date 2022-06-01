5663c44f838e12933ef47f0c6c66990499f02662
irving-Cesar <irvingcesar22@gmail.com>
2022-06-01 19:37:14 -0300

    func handleClick

--- src/index.js
+++ src/index.js
@@ -4,20 +4,13 @@ import './index.css';
 import { useState } from 'react';
 
 class Square extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: null
-        };
-    }
-
     render() {
         return (
             <button
                 className="square"
-                onClick={() => this.setState({ value: 'X' })}
+                onClick={() => this.props.onClick()}
             >
-                {this.state.value}
+                {this.props.value}
             </button>
         );
     }
@@ -30,8 +23,21 @@ class Board extends React.Component {
             squares: Array(9).fill(null),
         };
     }
+
+    handleClick (i) {
+        const squares = this.state.squares.slice();
+        squares[i] = 'X';
+        this.setState({squares: squares});
+        console.log(this.state.squares)
+    }
+
     renderSquare(i) {
-        return <Square value={this.state.squares[i]} />;
+        return (
+            <Square
+                value={this.state.squares[i]} 
+                onClick={() => this.handleClick(i)}
+            />
+        );
     }
 
     render() {
